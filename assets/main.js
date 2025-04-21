edf7578ea1fac5583e17893e5882ecc1e4c3eeef
Sasha <sashabustob@gmail.com>
2025-04-21 20:08:47 -0300

    pasar el desafio 2 de main.js a desafio2.html

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,1 @@
-const nombreProducto ="Royal Canin";
-let precioUnitario=500;
 
-let cantidadDeseada= prompt("¿Cuantas bolsas de "+nombreProducto+" desea comprar?");
-
-alert("Su total es: $"+(precioUnitario*cantidadDeseada));
